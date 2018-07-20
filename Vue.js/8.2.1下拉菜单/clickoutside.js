03dd38a3e14ac0c4b3b6ed696df4f442689ac884
TomatoDroid <770226915@qq.com>
2018-07-20 19:20:53 +0800

    '下拉菜单'

--- Vue.js/8.2.1下拉菜单/clickoutside.js
+++ Vue.js/8.2.1下拉菜单/clickoutside.js
@@ -4,14 +4,28 @@ Vue.directive('clickoutside',{
             if(el.contains(e.target)){
                 return false;
             }
+            // 如果自定义指令包含表达式，则执行
             if(binding.expression){
+                binding.value(e);
+            }
+        }
+        function documenEsc(e){
+            if(binding.modifiers.esc && e.keyCode === 27){
                 binding.value(e);
             }
         }
         el._vueClickOutSide_ = documentHandle;
         document.addEventListener('click',documentHandle);
+        el._vueEscOutSide_ = documenEsc;
+        document.addEventListener('keydown',documenEsc);
     },
-    unbind(){
+    // updated (el,binding,value,oldvalue) {
         
+    // },
+    unbind(el,binding){
+        document.removeEventListener('click',el._vueClickOutSide_);
+        delete el._vueClickOutSide_;
+        document.removeEventListener('keydown',el._vueEscOutSide_);
+        delete el._vueEscOutSide_;
     }
 });
--- Vue.js/8.2.1下拉菜单/index.js
+++ Vue.js/8.2.1下拉菜单/index.js
@@ -1,7 +1,7 @@
 var app = new Vue({
     el:"#app",
     data:{
-        show:flase
+        show:false
     },
     methods:{
         handleClose(){
