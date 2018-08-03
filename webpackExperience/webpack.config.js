cd0820766dc93acba222867d5135a0721ddd9d51
liuzhen068 <liuzhen068@deppon.com>
2018-08-03 19:26:56 +0800

    webpack
    webpack111

---
+++ webpackExperience/webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var config  = {
+    // 入口
+    entry:{
+        main:'./main'
+    },
+    // 出口  打包后的文件将会存储为。./dist/main.js
+    output:{
+        path: path.join(__dirname,'./dist'),
+        publicPath: '/dist/',
+        filename: 'main.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:ExtractTextPlugin.extract({
+                    use:'css-loader',
+                    fullback:'style-loader'
+                })
+            }
+        ]
+    },
+    plugins:[
+        //重命名提取后的css文件
+        new ExtractTextPlugin('main.css')
+    ]
+}
+
+//相当于exports default config  因为没有安装编译ES6的插件
+module.exports = config;
