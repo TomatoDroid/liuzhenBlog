37b4eb3a3a619251be03017c4d876f4da8a0388d
491266 <liuzhen068@deppon.com>
2018-09-20 19:36:42 +0800

    readStream

---
+++ node/nodeWeb/staticWeb/readStream.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs  =require('fs');
+var parse = require('url').parse;
+var join = require('path').join;
+
+var root = __dirname;
+var server = http.createServer(function(req,res){
+    var url = parse(req.url);
+    var path = join(root,url.pathname);
+    var stream = fs.createReadStream(path);
+    stream.on('data',function(chunk){
+        res.write(chunk);
+    });
+    stream.on('end',function(){
+        res.end();
+    });
+});
+
+server.listen(3000);
