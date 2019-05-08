9bedb12885ff9437baee0c032beef31ac5e1cd19
tomato <770226915@qq.com>
2019-05-08 10:48:42 +0800

    杂项

--- vueAdmin/vuecli3.0admin/vue-course/vue.config.js
+++ vueAdmin/vuecli3.0admin/vue-course/vue.config.js
@@ -5,16 +5,16 @@ const resolve = dir => path.join(__dirname, dir)
 const BASE_URL = process.env.NODE_ENV === 'prodection' ? '/' : '/'
 
 module.exports = {
-  lintOnSave: false,
-  publicPath: BASE_URL ,
-  chainWebpack: config => {
-    config.resolve.alias
-      .set('@', resolve('src'))
-      .set('_c', resolve('src/components'))
-  },
-  //打包时不生成map文件，减少打包体积，加快打包速度
-  productionSourceMap: false,
-  devServer:{
-    // proxy:'http://localhost:3000'
-  }
+	lintOnSave: false,
+	publicPath: BASE_URL,
+	chainWebpack: config => {
+		config.resolve.alias
+			.set('@', resolve('src'))
+			.set('_c', resolve('src/components'))
+	},
+	//打包时不生成map文件，减少打包体积，加快打包速度
+	productionSourceMap: false,
+	devServer: {
+		// proxy:'http://localhost:3000'
+	}
 }
